ed381a629dc8d9727abdbd5a7e583c026f2a0a13
LuisAlbe3 <llandeross2100@alumno.ipn.mx>
2024-11-27 03:25:30 -0600

    aggregue express

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');  // Para manejar rutas de archivos estáticos
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Servir archivos estáticos desde la raíz del proyecto
+app.use(express.static(path.join(__dirname)));
+
 // Cadena de conexión a MongoDB Atlas
-const url = "mongodb+srv://luis:<luis1234>@cluster0.szyc1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const url = 'mongodb://luis:luis1234@cluster0.mongodb.net/?ssl=true&replicaSet=atlas-55hazr-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0';
 
 const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -18,7 +22,7 @@ client.connect()
         const db = client.db('inventario');  // Nombre de la base de datos
         const productosCollection = db.collection('productos');  // Colección de productos
 
-        // Definir la ruta para obtener todos los productos
+        // Ruta para obtener todos los productos
         app.get('/inventario', async (req, res) => {
             try {
                 const productos = await productosCollection.find({}).toArray();  // Obtener productos
